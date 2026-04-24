4b3f1877c0000e03ee905a8f1fd29906c63cde12
Sharadindu Das <sharadindudas774@gmail.com>
2026-04-24 11:23:50 +0530

    chore: Refactor factorial functions to improve input validation and simplify logic

--- factorial-of-a-number.js
+++ factorial-of-a-number.js
@@ -1,48 +1,22 @@
-// Write a function to calculate the factorial of a given number
-function factorialOfANumber(input) {
-  if (typeof input !== "number") {
-    throw new Error("Input must be a number");
+function factorial(n) {
+  if(typeof n !== "number" || !Number.isInteger(n) || n < 0){
+    return false
   }
-
-  if (!Number.isFinite(input)) {
-    throw new Error("Input must be a finite number");
+  
+  let result = 1;
+  for(let i = 2; i <= n; i++){
+    result *= i
   }
-
-  if (!Number.isInteger(input) || input < 0) {
-    throw new Error("Input must be a positive integer");
-  }
-
-  let output = 1;
-
-  for (let i = 1; i <= input; i++) {
-    output *= i;
-  }
-  return output;
+  return result
 }
-console.log(factorialOfANumber(5));
 
-// Write a function to calculate factorial of a given number using recursion
-function factorialOfANumberUsingRecursion(input) {
-  if (typeof input !== "number") {
-    throw new Error("Input must be a number");
+function factorialUsingRecursion(n){
+  if(typeof n !== "number" || !Number.isInteger(n) || n < 0){
+    return false
   }
-
-  if (!Number.isFinite(input)) {
-    throw new Error("Input must be a finite number");
-  }
-
-  if (!Number.isInteger(input)) {
-    throw new Error("Input must be an integer");
-  }
-
-  if (input < 0) {
-    throw new Error("Input must be a positive number");
-  }
-
-  if (input === 0 || input === 1) {
-    return 1;
-  } else {
-    return input * factorialOfANumberUsingRecursion(input - 1);
-  }
+  
+  if(n === 0) return 1
+  
+  return n * factorialUsingRecursion(n - 1)
+  
 }
-console.log(factorialOfANumberUsingRecursion(5));
